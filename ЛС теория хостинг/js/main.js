b601fadbe0f245336c233dadb8385684389e58e0
KseniaBochkova <126641109+KseniaBochkova@users.noreply.github.com>
2023-05-16 17:18:04 +0300

    first version

---
+++ ЛС теория хостинг/js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+   document.getElementById("menu-mobile").addEventListener("click", function (){
+      document.querySelector("header").classList.toggle("open")
+   })
+})
